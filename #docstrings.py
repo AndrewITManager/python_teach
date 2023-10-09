ac9b87d0004400f47137fc032544a2f4cfe1d307
Andrew Akimov <lutijandrew@gmail.com>
2023-10-09 16:31:47 +0300

    docstring

---
+++ #docstrings.py
@@ -0,0 +1,13 @@
+#docstrings
+
+
+def mult_by_factor(value, mult=1):
+    """_summary_
+
+    Args:
+        value (_type_): _description_
+        mult (int, optional): _description_. Defaults to 1.
+    """
+    return value * mult
+
+mult_by_factor(5)

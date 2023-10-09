3accafdc9cb629b59fd76be2b8ae9e53c4d7fbbc
Andrew Akimov <lutijandrew@gmail.com>
2023-10-09 16:45:27 +0300

    new

--- #docstrings.py
+++ #docstrings.py
@@ -11,3 +11,10 @@ def mult_by_factor(value, mult=1):
     return value * mult
 
 mult_by_factor(5)
+
+
+def print_number_info(num):
+    if (num % 2) == 0:
+        print("Num is even")
+    else:
+        print("Num is odd")

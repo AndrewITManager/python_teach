88c0cac92d0595d9a5a2fc0021a6462711f6edda
Andrew Akimov <lutijandrew@gmail.com>
2023-10-11 15:48:23 +0300

    bool

--- #логические операторы.py
+++ #логические операторы.py
@@ -6,3 +6,10 @@ print(not '')
 print(not True)
 print(not None)
 print(not not None)
+
+#praktika
+my_list = [1, 2]
+other_list = ['a', 'b']
+print(my_list or other_list)
+print(len(my_list) > 0 or other_list)
+print(len(my_list) < 0 or other_list)

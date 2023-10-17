97b2f12179bbf39306dc4f9b01df10e0b580558e
Andrew <lutijandrew@gmail.com>
2023-10-17 23:50:41 +0300

    if else

---
+++ #инструкция if else.py
@@ -0,0 +1,19 @@
+#инструкция if else
+my_number = 21.5
+
+if type(my_number) is int:
+    print(my_number, "is integer")
+else:
+    print(my_number, "is not an integer")
+
+
+my_phone = {
+    'price': 200,
+    #'brand': 'nokia',
+}  
+
+if my_phone.get('brand'):
+    print("Phone's brand is", my_phone['brand'])
+else:
+    print("There is no phone brand")
+

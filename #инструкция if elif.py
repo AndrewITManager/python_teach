f85e3782d3fc76cc9213e8042c80b3ed5d1365a1
Andrew <lutijandrew@gmail.com>
2023-10-18 00:44:28 +0300

    new comment

--- #инструкция if elif.py
+++ #инструкция if elif.py
@@ -1,11 +1,24 @@
 #инструкция if in def
+# def nums_info(a, b):
+#     if (type(a) is not int) or (type(b) is not int):
+#         return "not integer"
+    
+#     if a >= b:
+#         return f"{a} > or = {b}" 
+#     return f"{a} < {b}"
+
+# print(nums_info(True, 10))
+# print(nums_info(10, 2))
+# print(nums_info(4, 15))
+
 def nums_info(a, b):
     if (type(a) is not int) or (type(b) is not int):
         return "not integer"
-    
-    if a >= b:
-        return f"{a} > or = {b}" 
-    return f"{a} < {b}"
+    elif a >= b:
+        info = f"{a} > or = {b}" 
+    else:
+        info = f"{a} < {b}"
+    return info
 
 print(nums_info(True, 10))
 print(nums_info(10, 2))

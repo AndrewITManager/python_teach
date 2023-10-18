67e5b1130510282d403143497f86e88577a846dd
Akimov Andrew <lutijandrew@gmail.com>
2023-10-18 09:25:02 +0300

    reshenie

--- #задача if..py
+++ #задача if..py
@@ -1,5 +1,17 @@
 #задача if..elif
 
 def route_info(info):
-  pass
+  if info.get('distance'):
+    return f"Distance to your destination is {info.get('distance')}"
+  elif info.get('speed') and info.get('time'):
+    return f"Distance to your destination is {(info.get('speed') * info.get('time'))}"
+  return f"No distance info is available"
+  
 
+info_dict = {
+  #'distance': 45,
+  #'speed': 5,
+  'time': 4,
+}
+
+print(route_info(info_dict))

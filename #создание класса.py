26d4287891a18928509e476c60fb01bd60db6c55
Akimov Andrew <lutijandrew@gmail.com>
2023-10-20 16:50:56 +0300

    cllass

---
+++ #создание класса.py
@@ -0,0 +1,12 @@
+#создание класса
+class Car:#имя класса
+    def move(self):#метод, self - это указание на определенны экземляр класса Car
+        print("Car is moving")
+
+    def stop(self):
+        print("Car stopped")
+
+my_car = Car()
+
+print(type(my_car))
+print(isinstance(my_car, Car))

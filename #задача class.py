7f94459c58484e963175c8536b828dff43589130
Andrew <lutijandrew@gmail.com>
2023-10-21 20:58:24 +0300

    задача class

---
+++ #задача class.py
@@ -0,0 +1,23 @@
+#задача class
+class Image:
+    def __init__(self, resolution, title, extension):#собственные методы создаются при создании экземляра класса
+        self.resolution = resolution
+        self.title = title
+        self.extension = extension
+
+    def resize(self, new_resolution):#метод
+        self.resolution = new_resolution #новое разрешение
+
+    def __str__(self):
+        return f"{self.title}.{self.extension}"
+
+first_img = Image('1920x1080', "My Dog", 'jpg')
+
+print(first_img.resolution)
+print(first_img.title)
+print(first_img.extension)
+
+first_img.resize('4000x3000')
+print(first_img.resolution)
+
+print(first_img)
--- #создание класса с методом _init_.py
+++ #создание класса с методом _init_.py
@@ -1,8 +1,33 @@
 #создание класса с методом _init_
 class Comment:
-    def __init__(self, text):#создание атрибутов
+    def __init__(self, text):#создание собственных атрибутов
         self.text = text
         self.votes_qty = 0
 
-    def upvote(self):
-        self.votes_qty += 1
+    def upvote(self, qty):
+        self.votes_qty += qty
+
+# first_comment = Comment("First comment")
+# print(first_comment.text)
+# print(first_comment.votes_qty)
+
+# print(first_comment.__dict__)
+
+# print(dir(first_comment)) #все атрибуты экземпляра
+
+my_comment = Comment("My comment")
+# print(dir(my_comment))
+# print(my_comment)
+# print(my_comment.text)
+# print(my_comment.votes_qty)
+my_comment.upvote = 10
+print(my_comment.votes_qty)
+
+second_comment = Comment("Second comment")
+second_comment.upvote(2)
+print(second_comment.votes_qty)
+
+
+
+
+

c37034787b6f0818aa125f9d53c0a4e7e8d1a56e
Akimov Andrew <lutijandrew@gmail.com>
2023-11-02 18:56:09 +0300

    zip

--- #задача запись чтение файла.py
+++ #задача запись чтение файла.py
@@ -4,8 +4,8 @@ from pathlib import Path #экземпляр класса
 files_dir_path = Path('files')
 files_dir_path.mkdir(exist_ok=True)#если такая ошибка есть то папка не будет сгенерирована
 
-first_file = Path(files_dir_path / 'first.txt')
-second_file = Path(files_dir_path / 'second.txt')
+first_file = files_dir_path / 'first.txt'
+second_file = files_dir_path / 'second.txt'
 
 with open(first_file, 'w') as f:
     f.write("First line\n")
@@ -13,9 +13,9 @@ with open(first_file, 'w') as f:
 
 with open(second_file, 'w') as f:
     lines = [
-        "First line in the second file",
-        "Second line in the second file",
-        "Last line in the second file"
+        " First line in the second file",
+        " Second line in the second file",
+        "Last line in the second file "
     ]
    
     for line in lines:
@@ -25,11 +25,18 @@ with open(first_file) as f:
     print(f.read())
 
 with open(second_file) as f:
-    for line in f.readlines():
-        print(line)
+    ### option one ####
+    for line in f:
+        print(line.strip())
+    ### option two ####    
+    # while True:
+    #     line = f.readline()
+    #     if not line:
+    #         break
+    #     print(line.strip())#метод strip убирает лишние пробелы и \n
 
-first_file.unlink()
-second_file.unlink()
+first_file.unlink()#удаляем файл first.txt в директории
+second_file.unlink()#удаляем файл second.txt в директории
 files_dir_path.rmdir()#удаляет только пустую директорию
 
 

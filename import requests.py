8893701567fe07a1b4aba654bd9b4d1ea2d54af4
Akimov Andrew <lutijandrew@gmail.com>
2023-11-07 23:58:40 +0300

    praktik

---
+++ import requests.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+link = "https://browser-info.ru/"
+responce = requests.get(link).text
+soup = BeautifulSoup(responce, 'lxml')
+block = soup.find('div', id = "tool_padding")
+chek_js = block.find('div', id = 'javascript_chek')
+
+
+
+
+
+with open("1.html", "w", encoding="utf-8") as file:
+    file.write(responce)

36e1f00d94123cb71ac347cc20c73ba9c4ad2fd7
Akimov Andrew <lutijandrew@gmail.com>
2023-11-09 23:26:40 +0300

    work dict

--- temp/my_dict.py
+++ temp/my_dict.py
@@ -8,3 +8,16 @@ my_dict = {
 
 print(my_dict)
 temp_dict = {}
+my_dict.update({'auto': 'LADA', 'moto': 'DUCATI'})#добавляет одну или несколько парз к - з
+print(my_dict)
+print(my_dict.get('name'))#выводит значение по ключу
+print(my_dict.get('other', 'No key'))#выводит значение по ключу, если отсутствует второй аргумент
+my_dict.pop('auto')#удаляет элемент по ключу
+print(my_dict)
+print(my_dict.keys())#выводит ключи без значений
+print(my_dict.values())#выводит значения без ключей
+print(my_dict.items())#выводит пары к - з
+dict_two = my_dict.copy()#копия словаря
+print(dict_two)
+my_dict.clear()#удаляет все из словаря
+print(my_dict)
